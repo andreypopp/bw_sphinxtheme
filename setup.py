5f09d56ecd6b3e42244ab44faeb5940a7e572319
Andrey Popp <8mayday@gmail.com>
2011-12-05 00:23:45 +0400

    add inclusion manifest

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ version = "0.1"
 setup(name="bw_sphinxtheme",
       version=version,
       description="Minimalistic sphinx theme in black and white colours",
+      long_description=open("README", "r").read(),
       author="Andrey Popp",
       author_email="8mayday@gmail.com",
       url="http://pypi.python.org/pypi/bw_sphinxtheme",

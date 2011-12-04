e6c9310ba2f8a94c8f7622ed811424d17fff00b1
Andrey Popp <8mayday@gmail.com>
2011-12-05 00:16:28 +0400

    make it a python package

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+from setuptools import find_packages
+
+version = "0.1"
+
+setup(name="bw_sphinxtheme",
+      version=version,
+      description="Minimalistic sphinx theme in black and white colours",
+      author="Andrey Popp",
+      author_email="8mayday@gmail.com",
+      url="http://pypi.python.org/pypi/bw_sphinxtheme",
+      license="BSD3",
+      packages=find_packages(exclude=["ez_setup", "examples", "tests"]),
+      zip_safe=False)
